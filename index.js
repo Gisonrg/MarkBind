57072c17e36eab3cf6cbdca5fed362d7dc1c948d
Jiang Sheng <gisonrg@gmail.com>
2016-12-21 00:30:21 +0800

    Update code

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 'use strict';
 
 // Entry file for Markbind project
@@ -5,8 +6,13 @@ const chalk = require('chalk');
 const clear = require('clear');
 const figlet = require('figlet');
 const fs = require('fs');
+const path = require('path');
+const program = require('commander');
 
 const Parser = require('./lib/parser');
+const utils = require('./lib/utils');
+
+let markbindParser = new Parser();
 
 clear();
 console.log(
@@ -15,9 +21,44 @@ console.log(
     )
 );
 
-let inputFile = '/home/jason/code/markbind/src/index.html';
+program
+  .version('0.1.0')
+  .option('-o, --output <path>', 'output file path')
+  .arguments('<file>')
+  .action(function (file) {
+  });
 
-let markbindParser = new Parser();
-markbindParser.parseFile(inputFile, (error, html) => {
-   console.log(html);
-});
+program
+  .command('include <file>')
+  .description('process all the include in the given file')
+  .action(function(file) {
+    let output = program.output || 'output';
+    markbindParser.fileInclude(file, (error, html) => {
+      fs.writeFileSync(path.join(process.cwd(), path.parse(file).base), html);
+    });
+  });
+
+program
+  .command('render <file>')
+  .description('render the given file')
+  .action(function(file) {
+    // check file existence
+    let output = program.output || 'output';
+    markbindParser.renderFile(file, (error, html) => {
+      fs.writeFileSync(path.join(process.cwd(), path.parse(file).name + '.html'), html);
+    });
+  });
+
+if (typeof cmdValue === 'undefined') {
+  // Handle illegal stuff
+  // process.exit(1);
+}
+
+program.parse(process.argv);
+
+// let inputFile = '/Users/Gison/code/2016/FYP/markbind/src/index.html';
+
+// markbindParser.parseFile(inputFile, (error, html) => {
+//   console.log(html);
+//   fs.writeFileSync('out.html', html);
+// });
